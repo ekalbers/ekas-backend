9e9424df8041b127e9f28bb0a5ae2d7d2467bc74
Ethan Albers <ekalbers22@gmail.com>
2023-07-25 19:29:36 -0400

    setup static files

--- backend/urls.py
+++ backend/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from backend.views import get_csrf_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inforequests/', include('ek_drones.urls')),
     path('get-csrf-token/', get_csrf_token, name='get-csrf-token')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
