95288b1266a27a2230c4b91c46387d1c79847e08
Ethan Albers <ekalbers22@gmail.com>
2023-07-25 18:04:53 -0400

    vercel setup

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+#
+# application = get_wsgi_application()
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
+app = application
