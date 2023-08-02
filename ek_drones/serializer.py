5e905d22a118e535ef9eba1fd4fa5aaa89e1e1ad
Ethan Albers <ekalbers22@gmail.com>
2023-08-02 00:38:31 -0400

    added default values for model

--- ek_drones/serializer.py
+++ ek_drones/serializer.py
@@ -6,3 +6,9 @@ class InfoRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = InfoRequest
         exclude = ['created_at']
+        fields = ['name', 'company', 'email', 'phone', 'message']
+        name = serializers.CharField(allow_blank=True)
+        company = serializers.CharField(allow_blank=True)
+        email = serializers.CharField(allow_blank=True)
+        phone = serializers.CharField(allow_blank=True)
+        message = serializers.CharField(allow_blank=True)
