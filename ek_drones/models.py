36fa63929ca987ccdc02510f6d372b83ef14988b
Ethan Albers <ekalbers22@gmail.com>
2023-08-02 00:23:16 -0400

    added default values for model

--- ek_drones/models.py
+++ ek_drones/models.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 
 
 class InfoRequest(models.Model):
-    name = models.CharField(max_length=32)
-    company = models.CharField(max_length=32)
-    email = models.CharField(max_length=32)
-    phone = models.CharField(max_length=16)
-    message = models.TextField()
+    name = models.CharField(max_length=32, default='None')
+    company = models.CharField(max_length=32, default='None')
+    email = models.CharField(max_length=32, default='None')
+    phone = models.CharField(max_length=16, default='None')
+    message = models.TextField(default='None')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
